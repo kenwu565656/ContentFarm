0a56328ff3fa0009d64816f0ae8fea5bd0d87230
Wu Tsz Lok <kenwu@WudeMac-mini.local>
2023-11-07 01:25:06 +0800

    first commit

---
+++ src/main/java/com/content/contentFarm/ContentFarmApplication.java
@@ -0,0 +1,13 @@
+package com.content.contentFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/content/contentFarm/ContentFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content.contentFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
